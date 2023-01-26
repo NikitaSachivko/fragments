219ea5875df0980e3c66c4a27bad0a92a88ea0df
Nikita Sachivko <nsachivko@myseneca.ca>
2023-01-26 18:00:40 -0500

    Triggering workflow error

--- tests/unit/app.test.js
+++ tests/unit/app.test.js
@@ -4,7 +4,7 @@ const app = require('../../src/app')
 describe("Test for triggering a 404 error", () => {
   test('Calling the wrong route', async () => {
     const res = await request(app).get('/wrong_route')
-    expect(res.statusCode).toBe(404)
+    expect(res.statusCode).toBe(500)
   }
   )
 })
