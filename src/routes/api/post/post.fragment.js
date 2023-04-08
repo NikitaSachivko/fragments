b0d97f3c0d02e4448f74759b51ba9dce8c164e72
Nikita Sachivko <nsachivko@myseneca.ca>
2023-04-07 20:48:22 -0400

    added integration test lab 10 + ci test

--- src/routes/api/post/post.fragment.js
+++ src/routes/api/post/post.fragment.js
@@ -31,12 +31,12 @@ module.exports = async (req, res) => {
     fragment.save()
     logger.info("Fragment saved: %s", fragment.id)
   } catch (err) {
-    logger.error(`Can not create new fragment`)
+    logger.error(`Can not create new fragment: ${err}`)
 
     res
       .status(422)
       .json(
-        createErrorResponse(422, "Can not create new fragment",)
+        createErrorResponse(422, `Can not create new fragment`,)
       )
     return
   }
